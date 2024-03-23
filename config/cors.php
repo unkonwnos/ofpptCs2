2dbcf50c26e1183ff8372f57229a1cdd9c0d0ff0
Winnery05 <elmedhassan2002@gmail.com>
2024-03-23 12:39:51 +0000

    allowed cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://ofppt-front-end.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
